bd56bac88d15122808279fa069b0522b9e4fd98e
Josh Akeman <joshakeman@gmail.com>
2019-05-16 10:49:28 -0400

    closed and open button tests working

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+import 'react-testing-library/cleanup-after-each'
+
+import Display from './Display'
+
+describe('<Display />',  () => {
+    it('Displays closed if the closed prop is true', () => {
+        const { getByText } = render(<Display closed />)
+
+        getByText(/Closed/i)
+    })
+    it('Displays open if the closed prop is false', () => {
+        const { getByText } = render(<Display closed={false}/>)
+
+        getByText(/Open/i)
+    })
+})
