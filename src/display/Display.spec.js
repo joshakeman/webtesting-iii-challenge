b7e32e8f2d1e47a85ca8cbd9cbb0807ba23311d4
Josh Akeman <joshakeman@gmail.com>
2019-05-16 11:07:16 -0400

    class check for closed/open and locked/unlocked passing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { render, fireEvent } from 'react-testing-library'
 import 'react-testing-library/cleanup-after-each'
+import 'jest-dom/extend-expect'
+import { toHaveClass } from 'jest-dom'
 
 import Display from './Display'
 
@@ -25,5 +27,29 @@ describe('<Display />',  () => {
         const { getByText } = render(<Display closed={false}/>)
 
         getByText(/unlocked/i)
+    })
+    it('Should have red-led class when locked', () => {
+        const { getByText } = render(<Display locked/>)
+        const button = getByText(/locked/i)
+
+        expect(button).toHaveClass('red-led')
+    })
+    it('Should have red-led class when closed', () => {
+        const { getByText } = render(<Display closed/>)
+        const button = getByText(/closed/i)
+
+        expect(button).toHaveClass('red-led')
+    })
+    it('Should have green-led class when unlocked', () => {
+        const { getByText } = render(<Display locked={false}/>)
+        const button = getByText(/unlocked/i)
+
+        expect(button).toHaveClass('green-led')
+    })
+    it('Should have green-led class when open', () => {
+        const { getByText } = render(<Display closed={false}/>)
+        const button = getByText(/open/i)
+
+        expect(button).toHaveClass('green-led')
     })
 })
